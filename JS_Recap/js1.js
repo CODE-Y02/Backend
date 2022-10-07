57f5eff8ad824fa2c450500b44be6b8c63376d94
yatharth <lakhatey@gmail.com>
2022-10-07 15:36:12 +0530

    Arrow Fun

--- JS_Recap/js1.js
+++ JS_Recap/js1.js
@@ -6,7 +6,7 @@ const hasHobbies = true;
 // name = "code_y";   --> type error cannot reassign to const
 age = 23;
 
-const summarizeUser = function (userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
   return (
     "Name is " +
     userName +
@@ -16,5 +16,12 @@ const summarizeUser = function (userName, userAge, userHasHobby) {
     userHasHobby
   );
 };
+
+// const add = (a, b) => a + b;
+const addOne = (a) => a + 1;
+const addRandom = () => 1 + 4;
+// console.log(add(1, 2));
+console.log(addOne(7));
+console.log(addRandom());
 
 console.log(summarizeUser(name, age, hasHobbies));
