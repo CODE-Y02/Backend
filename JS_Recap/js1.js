b26090489af84bdf3a8cee668205182e06b30105
yatharth <lakhatey@gmail.com>
2022-10-07 15:43:15 +0530

    OBJECT : problem with " arrow function and this "

--- JS_Recap/js1.js
+++ JS_Recap/js1.js
@@ -1,27 +1,39 @@
-const name = "yatharth";
-// console.log(name);
+// const name = "yatharth";
+// // console.log(name);
 
-let age = 24;
-const hasHobbies = true;
-// name = "code_y";   --> type error cannot reassign to const
-age = 23;
+// let age = 24;
+// const hasHobbies = true;
+// // name = "code_y";   --> type error cannot reassign to const
+// age = 23;
 
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies : " +
-    userHasHobby
-  );
+// const summarizeUser = (userName, userAge, userHasHobby) => {
+//   return (
+//     "Name is " +
+//     userName +
+//     ", age is " +
+//     userAge +
+//     " and the user has hobbies : " +
+//     userHasHobby
+//   );
+// };
+
+// // const add = (a, b) => a + b;
+// const addOne = (a) => a + 1;
+// const addRandom = () => 1 + 4;
+// // console.log(add(1, 2));
+// console.log(addOne(7));
+// console.log(addRandom());
+
+// console.log(summarizeUser(name, age, hasHobbies));
+
+// Objects
+const person = {
+  name: "yatharth",
+  age: 24,
+  greet: () => {
+    console.log("hi, i am " + this.name);
+  },
 };
 
-// const add = (a, b) => a + b;
-const addOne = (a) => a + 1;
-const addRandom = () => 1 + 4;
-// console.log(add(1, 2));
-console.log(addOne(7));
-console.log(addRandom());
-
-console.log(summarizeUser(name, age, hasHobbies));
+console.log(person);
+person.greet();
