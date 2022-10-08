b753b6d61d97685b1c4750605f5d180a429aa5f9
yatharth <lakhatey@gmail.com>
2022-10-08 18:36:37 +0530

    Sending reaponse

--- NodeBasic/app.js
+++ NodeBasic/app.js
@@ -8,6 +8,15 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers)
     // process.exit(); // used to de attach event 
+
+
+    //creating response
+    res.setHeader('Content-Type', "text/html");
+    res.write("<html>");
+    res.write("<head><title>My first Site</title></head>")
+    res.write("<body><h1>Hello from My  NOde server :) </h1></body>")
+    res.write("</html>")
+    res.end()
 });
 // create server acts as event listner 
 server.listen(3000)  
