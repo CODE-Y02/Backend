2c53970e0ec57c5ed553c0d300cd1f3d19ba7a36
yatharth <lakhatey@gmail.com>
2022-10-09 10:50:51 +0530

    POST REQ 1

--- NodeBasic/app.js
+++ NodeBasic/app.js
@@ -6,9 +6,18 @@ const http = require("http");
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+    // console.log(req.url, req.method, req.headers)
     // process.exit(); // used to de attach event 
 
+    const url = req.url;
+    if (url === "/") {
+        res.setHeader('Content-Type', "text/html");
+        res.write("<html>");
+        res.write("<head><title>My first Site</title></head>")
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'> <button type = 'submit'>Submit</button></form></body>")
+        res.write("</html>")
+        return res.end()
+    }
 
     //creating response
     res.setHeader('Content-Type', "text/html");
