50adf286dbdd6ff7f75ee98feeac422e6f067c0a
yatharth <lakhatey@gmail.com>
2022-10-09 10:59:56 +0530

    Redirecting after Post req

--- NodeBasic/app.js
+++ NodeBasic/app.js
@@ -3,19 +3,27 @@
 // we need to import module to import their functanility
 
 const http = require("http");
-
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers)
     // process.exit(); // used to de attach event 
 
     const url = req.url;
+    const method = req.method;
     if (url === "/") {
         res.setHeader('Content-Type', "text/html");
         res.write("<html>");
         res.write("<head><title>My first Site</title></head>")
         res.write("<body><form action='/message' method='POST'><input type='text' name='message'> <button type = 'submit'>Submit</button></form></body>")
         res.write("</html>")
+        return res.end()
+    }
+    //redirecting
+    if(url=== "/message"  && method === 'POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode= 302;
+        res.setHeader('Location','/')
         return res.end()
     }
 
