aad5bb2a840477eac06d4a7190c00bb6f500ebc3
yatharth <lakhatey@gmail.com>
2022-10-10 19:56:50 +0530

    MiddleWare

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -5,6 +5,17 @@ const express = require("express");
 //express app
 const app = express();
 
+//middle ware
+app.use((req, res, next) => {
+  console.log("In the middlewere");
+  next(); //allow to go to next middleware in lines
+});
+
+app.use((req, res, next) => {
+  console.log("In the Another middlewere");
+  // ...
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
