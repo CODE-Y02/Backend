a2c3b03e03e7395f2128842b6d80b0a7853f8486
yatharth <lakhatey@gmail.com>
2022-10-10 21:38:44 +0530

    removing http import and using express app instead

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -14,10 +14,11 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log("In the Another middlewere");
 
-  res.send("<h1>Hello from express</h1>");
+  res.send("<html><h1>Hello from express</h1></html>");
 });
 
 // const server = http.createServer(app);
 
 // server.listen(3000);
+
 app.listen(3000);
