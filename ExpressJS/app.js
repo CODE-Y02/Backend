1af4ce9ab783a8bcf984fd277b6f10abfd585c28
yatharth <lakhatey@gmail.com>
2022-10-11 10:43:57 +0530

    Routing withh middleware 1

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -6,23 +6,20 @@ const express = require("express");
 const app = express();
 
 //middle ware
-app.use((req, res, next) => {
-  console.log("In the middlewere");
-  next(); //allow to go to next middleware in lines
+
+app.use("/", (req, res, next) => {
+  console.log("MiddleWare 1 always runs");
+  next();
 });
 
-app.use((req, res, next) => {
-  console.log("In the Another middlewere");
-
-  res.send("<html><h1>Hello from express</h1></html>");
-  // this give content type text html
-
-  // res.send({ hello: "Yatharth" });
-  // gives content type applicatiom/json
+app.use("/add-product", (req, res, next) => {
+  console.log("MiddleWare 2");
+  res.send("<h1>add-product</h1>");
 });
 
-// const server = http.createServer(app);
-
-// server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("MiddleWare 3");
+  res.send("<h1>Hello from express</h1>");
+});
 
 app.listen(3000);
