069c47f0e59447e90e91965edec910496e1ae4f9
yatharth <lakhatey@gmail.com>
2022-10-12 18:13:49 +0530

    Improverd some logic and re write code

--- Projects/chatApp/app.js
+++ Projects/chatApp/app.js
@@ -1,17 +1,61 @@
 const express = require("express");
+const fs = require("fs");
 const bodyParser = require("body-parser");
-
-//import routes
-const login = require("./routes/login");
-const chats = require("./routes/chats");
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(login);
+//create login
+app.get("/login", (req, res, next) => {
+  res.send(`
+    <form
+  action="/"
+  method="post"
+  onsubmit='localStorage.setItem("username", document.getElementById("username").value)'
+>
+  <label for="username"> Enter Username </label>
+  <input type="text" id="username" name="username"/>
+  <button type="submit">Login</button>
+</form>`);
+});
 
-app.use(chats);
+app.get("/", (req, res, next) => {
+  //read from file
+  fs.readFile("allChats.txt", (error, data) => {
+    if (error && !data) data = "No Messages";
+    else if (error) console.log(error); // when theres data but some error occur
 
-//listen to port
+    //data -- > all msg
+    res.send(`
+    <p>${data}</p>
+    <form
+    action="/"
+    method="post"
+    onsubmit='  document.getElementById("username").value = localStorage.getItem("username")'
+  >
+    <label for="msg"> Enter Message </label>
+    <input type="text" id="msg" name="message" />
+    <input type="hidden" id="username" name="username" />
+    <button type="submit">Send</button>`);
+  });
+});
+
+app.post("/", (req, res) => {
+  // when we do not need next in args we can skip
+
+  if (!req.body.username) return res.redirect("/login");
+  if (!req.body.message) return res.redirect("/");
+
+  fs.writeFile(
+    "allChats.txt",
+    `${req.body.username}:${req.body.message} \s`,
+    { flag: "a" },
+    (err) => {
+      if (err) console.log(err);
+
+      res.redirect("/");
+    }
+  );
+});
+
 app.listen(3000);
