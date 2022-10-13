3b9aeedd7e195a80addd6eab951507244c01945b
yatharth <lakhatey@gmail.com>
2022-10-13 21:53:04 +0530

    improved path in routes

--- ExpressJS/routes/shop.js
+++ ExpressJS/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // using get and post insted of use make sures that " path " is exact match
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"), (err) => {
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"), (err) => {
     console.log(err);
   });
 });
