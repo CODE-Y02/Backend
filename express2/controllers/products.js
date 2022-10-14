879ce76d8ef64ea9e2b903372a3571ad326fca8b
yatharth <lakhatey@gmail.com>
2022-10-14 21:21:03 +0530

    adding controller 2

--- express2/controllers/products.js
+++ express2/controllers/products.js
@@ -1,3 +1,5 @@
+const products = [];
+
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
@@ -12,4 +14,15 @@ exports.getAddProduct = (req, res, next) => {
 module.exports.postAddProduct = (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect("/");
+};
+
+exports.getProduct = (req, res, next) => {
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 };
