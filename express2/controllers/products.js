390cbd695d4eeea6064b3a70fec44b72566a5074
yatharth <lakhatey@gmail.com>
2022-10-14 21:20:49 +0530

    Adding controllers

---
+++ express2/controllers/products.js
@@ -0,0 +1,15 @@
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+// same things module.exports = getAddProduct
+
+module.exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+};
---
+++ express2/routes/admin.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const express = require("express");
+
+const productsController = require("../controllers/products");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get("/add-product", productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post("/add-product");
+
+exports.routes = router;
+exports.products = products;
