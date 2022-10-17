ddacc035f2e1dbac82307556071090fa5dcdd18d
yatharth <lakhatey@gmail.com>
2022-10-17 13:45:34 +0530

    Adding Cart functiionality

--- DynamicRoute/controllers/shop.js
+++ DynamicRoute/controllers/shop.js
@@ -41,6 +41,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  //get prodID
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
