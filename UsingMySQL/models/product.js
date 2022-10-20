237b64c437a6354f17a75c1394a3f0d8feb308a1
yatharth <lakhatey@gmail.com>
2022-10-20 19:38:47 +0530

    Created product model with sequelize

--- UsingMySQL/models/product.js
+++ UsingMySQL/models/product.js
@@ -1,44 +1,32 @@
-const Cart = require("./cart");
+const Sequelize = require("sequelize");
 
-// import db promise from dabase config file
-const db = require("../util/database");
-const e = require("express");
+//import Sequelize db pool
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(title, imageUrl, description, price, id = null) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+// create new model
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    if (this.id) {
-      // product alread exist update it
-      return db.execute(
-        `UPDATE products SET title=?,description=?,price=?,imageUrl=? WHERE id=?`,
-        [this.title, this.description, this.price, this.imageUrl, this.id]
-      );
-    } else {
-      // create new product
-      return db.execute(
-        `INSERT INTO products (title,description,price,imageUrl) VALUES (?,?,?,?)`,
-        [this.title, this.description, this.price, this.imageUrl]
-      );
-    }
-  }
-
-  static fetchAll() {
-    // we are returning promise we can use .then catch with it
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE id=?", [id]);
-  }
-
-  static deleteproductbyID(id) {
-    return db.execute("DELETE FROM products WHERE id=?", [id]);
-  }
-};
+module.exports = Product;
