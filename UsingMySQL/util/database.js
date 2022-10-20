824ec718703f4d055f2c6cc60908b0aea702b90e
yatharth <lakhatey@gmail.com>
2022-10-20 19:21:08 +0530

    Sequelize 1

--- UsingMySQL/util/database.js
+++ UsingMySQL/util/database.js
@@ -1,14 +1,10 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-//we have 2 ways to create connection , one is create single connection for each query and other is creating pool
-// creating pool is efficient
+// with Sequelize we dont neewd to write sql queries manually
 
-// pool manages multiple connction
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "King@Ninja@123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root", //by default
-  database: "node-complete", //db name or schema name
-  password: "King@Ninja@123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
