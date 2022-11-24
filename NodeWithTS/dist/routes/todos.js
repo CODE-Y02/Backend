168805b3dc677c8711bc267b6a354f9b3d28e93c
yatharth <lakhatey@gmail.com>
2022-11-24 23:39:54 +0530

    BETTER FOLDER STRUCTURE

--- NodeWithTS/dist/routes/todos.js
+++ NodeWithTS/dist/routes/todos.js
@@ -10,29 +10,34 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post("/todo", (req, res, next) => {
+    // const body = req.body as { text: string };
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ message: " added todo ", newTodo, todos });
 });
 router.put("/todo/:todoId", (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params;
+    const tid = params.todoId;
+    const body = req.body;
     const todoIdx = todos.findIndex((todoItem) => todoItem.id === tid);
     if (todoIdx >= 0) {
         // todo found
         todos[todoIdx] = {
             id: todos[todoIdx].id,
-            text: req.body.text,
+            text: body.text,
         };
         return res.status(200).json({ message: " Item updated successfully" });
     }
     res.status(404).json({ message: " Item Not fount for given Id" });
 });
 router.delete("/todo/:todoId", (req, res, next) => {
+    const params = req.params;
     let oldLen = todos.length;
-    todos = todos.filter((eachTodo) => eachTodo.id !== req.params.todoId);
+    todos = todos.filter((eachTodo) => eachTodo.id !== params.todoId);
     if (todos.length === oldLen) {
         //no ele is removed
         return res.status(404).json({ message: " Item Not fount for given Id" });
