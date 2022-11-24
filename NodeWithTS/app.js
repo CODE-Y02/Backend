35fc4add720163ddb887b3b24c7de215f60a7702
yatharth <lakhatey@gmail.com>
2022-11-24 19:25:53 +0530

    fix

---
+++ NodeWithTS/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// import express from "express";
+//possible syntax
+// import express = require("express");
+const express = require("express");
+const app = express();
+app.listen(3000);
